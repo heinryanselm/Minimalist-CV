688936540b4f90338a32603cbffe53b4517079d5
Heinry-Anselm <50972784+heinryanselm@users.noreply.github.com>
2024-10-09 20:59:38 +0000

    More update to resume data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -159,7 +159,7 @@ export default function Page() {
           })}
         </Section>
         <Section>
-          <h2 className="text-xl font-bold">Skills</h2>
+          <h2 className="text-xl font-bold">Key Technologies</h2>
           <div className="flex flex-wrap gap-1">
             {RESUME_DATA.skills.map((skill) => {
               return <Badge key={skill}>{skill}</Badge>;
--- src/data/resume-data.tsx
+++ src/data/resume-data.tsx
@@ -6,9 +6,9 @@ export const RESUME_DATA = {
   location: "Hull, England",
   locationLink: "https://www.google.com/maps/place/Hull,+England/@53.7446794,-0.3",
   about:
-    "Software Engineer, Frontend & App Developer, focused on building products with extra attention to detail",
+    "Senior Frontend Developer",
   summary:
-    "Prolific software engineer, I have successfully taken multiple products from 0 to 1. I lead teams effectively, ensuring an environment where people can do their best work. Currently, I work mostly with TypeScript, React, Node.js, and GraphQL.",
+    "Professional Googler & Senior Frontend Developer with a knack for piecing together the web one well-placed Stack Overflow thread at a time. With extensive experience in JavaScript, TypeScript, React, and Next.js, I’ve led projects that range from building seamless user interfaces to developing scalable web applications for high-impact solutions. My problem-solving method? Google first, coffee second. Whether it’s optimizing performance, mentoring junior developers, or delivering user-friendly designs, I’m all about crafting solutions that not only work but elevate the user experience (with a little search engine magic). I’m excited to bring my blend of frontend expertise, leadership, and humor to drive impactful, engaging digital experiences.",
   avatarUrl: "https://avatars.githubusercontent.com/u/50972784?s=400&u=1b5e251a7df70e707b2674ba3df87943fc7c1654&v=4",
   personalWebsiteUrl: "https://cv-ashy-nine.vercel.app",
   contact: {
@@ -37,6 +37,16 @@ export const RESUME_DATA = {
   ],
   work: [
     {
+      company: "Tech Educators",
+      link: "https://techeducators.co.uk/who-we-are/our-team",
+      badges: ["Full-time"],
+      title: "Software Development TA / Location Lead",
+      start: "03/2024",
+      end: "Present",
+      description:
+        "I teach and mentor junior developers in building mobile-first, accessible full-stack apps using React, Next.js, and more, while giving feedback that sharpens their coding skills. I lead breakout sessions, provide one-on-one support, and collaborate with the instructor team to keep everyone on track. On top of that, I help update the curriculum, build tech communities in Hull, and mentor students into tech roles, making sure they're industry-ready!",
+    },
+    {
       company: "iFix Network LTD",
       link: "https://www.ifixnetworks.net",
       badges: ["Full-time"],
@@ -44,16 +54,16 @@ export const RESUME_DATA = {
       start: "01/2021",
       end: "12/2023",
       description:
-        "Lead product definition, launch, and long-term vision for initiatives across audiences of the marketplace, enabling the brand to reach consumers nationwide by developing locally relevant strategies. Entrusted by Senior Executives with leadership responsibility on high-impact, high visibility projects.",
+        "I spearheaded the creation of eight impactful apps, boosting product performance and business growth like a pro. Designed a mobile app that made real-time supply chain monitoring a breeze, while also crafting slick UI components that made everything more accessible and user-friendly. Led the dev cycles from start to finish, mentoring junior devs and keeping things running smooth with React, Next.js, TypeScript, and more!",
     },
   ],
   skills: [
-    "JavaScript",
-    "TypeScript",
-    "React/Next.js",
-    "Node.js",
-    "Flutter",
-    "GraphQL",
+    "Frontend: JavaScript, TypeScript, HTML, CSS, React.js, Next.js",
+    "Backend/Full-stack: Node.js, RESTful APIs, GraphQL",
+    "Version Control & CI/CD: Git, GitHub, Docker, Netlify, Vercel",
+    "Testing & Debugging: Cypress, Chrome DevTools",
+    "Build Tools: Webpack, Babel, Vite",
+    "Performance & Accessibility: Lighthouse, Web Vitals",
   ],
   projects: [
     {
@@ -78,7 +88,7 @@ export const RESUME_DATA = {
       techStack: ["Side Project", "TypeScript", "Next.js", "Browser Extension"],
       description:
         "Browser extension that records everything happening in a web application",
-      // logo: MonitoLogo,
+      //logo: MonitoLogo,
       link: {
         label: "monito.dev",
         href: "https://monito.dev/",
